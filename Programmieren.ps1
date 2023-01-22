52f118721f00a3e484d0b332b8933fb67a4711a6
vinpuch <94136786+vinpuch@users.noreply.github.com>
2023-01-22 17:55:56 +0100

    Add files via upload

---
+++ Programmieren.ps1
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+start "C:\Program Files\Google\Chrome\Application\chrome.exe"
+start "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\IntelliJ IDEA 2022.3.1.lnk"
+timeout /t 2
+msg * "Viel Erfolg beim Lernen!"
